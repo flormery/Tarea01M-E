dc8a91540ed8b4fcf5e859d212622f0d821a7272
flormery <meryelizabeth19@gmail.com>
2022-05-19 18:00:35 +0000

    ejercicio410

--- EstRepetitivas/ejercicio410.java
+++ EstRepetitivas/ejercicio410.java
@@ -8,19 +8,23 @@ import java.util.Scanner;
 public class ejercicio410 {
     static Scanner leerT=new Scanner(System.in);
 
-    public static int factorialN(int numero) {
-        int resultado=1;
+    public static long factorialN(int numero) {
+        long resultado=1;
         if ( numero>1){
             for (int i = 1; i <= numero; i++) {
                 resultado=resultado*i;
                 
             }
         }
-             
         return resultado; 
     }
 
     public static void main(String[] args) {
-       System.out.println(factorialN(5)); 
+        System.out.println("Ingrese un numero:");
+        int num=leerT.nextInt();
+       //System.out.println(factorialN(num)); 
+       for (int i = 0; i <= num; i++){
+           System.out.println("factorial de "+ i+" es:"+factorialN(i));
+       }
     }
 }
