cc1eee2f289143754b18aa8207124f13ddd36f97
flormery <meryelizabeth19@gmail.com>
2022-05-19 17:29:22 +0000

    ejercicios410

---
+++ EstRepetitivas/ejercicio410.java
@@ -0,0 +1,26 @@
+package EstRepetitivas;
+
+import java.util.Scanner;
+
+/**
+ * ejercicio410
+ */
+public class ejercicio410 {
+    static Scanner leerT=new Scanner(System.in);
+
+    public static int factorialN(int numero) {
+        int resultado=1;
+        if ( numero>1){
+            for (int i = 1; i <= numero; i++) {
+                resultado=resultado*i;
+                
+            }
+        }
+             
+        return resultado; 
+    }
+
+    public static void main(String[] args) {
+       System.out.println(factorialN(5)); 
+    }
+}
