8f5bdddbbec36cc3b519dc17cec9cd3161568a90
flormery <meryelizabeth19@gmail.com>
2022-05-28 20:20:42 +0000

    tarea4

---
+++ tareaFP/ejercicio41.java
@@ -0,0 +1,24 @@
+package tareaFP;
+
+import java.util.Scanner;
+
+/**
+ * ejercicio
+ */
+public class ejercicio41 {
+
+    static Scanner it=new Scanner (System.in);
+    public static void ejercicio41() {
+        //Definir variables
+        double total=1500;
+        //Proceso
+        for(int i = 0; i< 6; i++){
+            total=total+(total*0.10);
+            System.out.println("su salario es"+(i+i)+"año sera de: "+total);
+        }
+        
+    }
+    public static void main(String[] args) {
+        ejercicio41();
+    }
+    }
